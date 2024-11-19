78c6e0676313d660ca68b95dac2cd80511facb6f
paullarasales <ahmadzufukli@gmail.com>
2024-11-20 00:58:14 +0800

    done with the user saving into the database

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,9 @@
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { checkUser } from "@/lib/checkUser";
 
-const Header = () => {
+const Header = async () => {
+    const user = await checkUser();
+
     return (<nav className="navbar">
         <div className="navbar-container">
             <h2>Expense Tracker</h2>
