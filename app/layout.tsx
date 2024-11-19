56acb8b8d45b0032eb90b4e40a6dcf1c80158aa8
paullarasales <ahmadzufukli@gmail.com>
2024-11-20 00:40:00 +0800

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,10 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import { Poppins } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import Header from "@/components/Header";
+import './globals.css';
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const poppins = Poppins({ weight: "500", subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,10 +17,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${poppins.className} ${poppins.className}`}>
+          <Header />
+          <main className="container">
+            {children}
+          </main>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
